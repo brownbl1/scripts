e0464e0f177d54734b17c59b9c8bcddcccb411d1
Ben Brown <itslittlejohn@gmail.com>
2019-06-11 11:27:21 -0400

    add modified git prompt to add git status colors

---
+++ git-prompt.sh
@@ -0,0 +1,75 @@
+COLOR_RED="\033[0;31m"
+COLOR_YELLOW="\033[0;33m"
+COLOR_GREEN="\033[0;32m"
+COLOR_OCHRE="\033[38;5;95m"
+COLOR_BLUE="\033[0;34m"
+COLOR_WHITE="\033[0;37m"
+COLOR_RESET="\033[0m"
+
+function git_color {
+  local git_status="$(git status 2> /dev/null)"
+
+  if [[ $git_status =~ "nothing to commit, working tree clean" ]]; then
+    echo -e $COLOR_WHITE
+  # elif [[ $git_status =~ "Your branch is ahead of" ]]; then
+  #   echo -e $COLOR_YELLOW
+  # elif [[ $git_status =~ "nothing to commit" ]]; then
+  #   echo -e $COLOR_GREEN
+  else
+    echo -e $COLOR_RED
+  fi
+}
+
+
+
+if test -f /etc/profile.d/git-sdk.sh
+then
+	TITLEPREFIX=SDK-${MSYSTEM#MINGW}
+else
+	TITLEPREFIX=$MSYSTEM
+fi
+
+if test -f ~/.config/git/git-prompt.sh
+then
+	. ~/.config/git/git-prompt.sh
+else
+	PS1='\[\033]0;$TITLEPREFIX:$PWD\007\]' # set window title
+	PS1="$PS1"'\n'                 # new line
+	PS1="$PS1"'\[\033[32m\]'       # change to green
+	PS1="$PS1"'\u@\h '             # user@host<space>
+	PS1="$PS1"'\[\033[35m\]'       # change to purple
+	# PS1="$PS1"'$MSYSTEM '          # show MSYSTEM
+	PS1="$PS1"'\[\033[33m\]'       # change to brownish yellow
+	PS1="$PS1"'\w'                 # current working directory
+	if test -z "$WINELOADERNOEXEC"
+	then
+		GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
+		COMPLETION_PATH="${GIT_EXEC_PATH%/libexec/git-core}"
+		COMPLETION_PATH="${COMPLETION_PATH%/lib/git-core}"
+		COMPLETION_PATH="$COMPLETION_PATH/share/git/completion"
+		if test -f "$COMPLETION_PATH/git-prompt.sh"
+		then
+			. "$COMPLETION_PATH/git-completion.bash"
+			. "$COMPLETION_PATH/git-prompt.sh"
+			PS1="$PS1"'`git_color`'   # colors git status
+			# PS1="$PS1"'\[\033[36m\]'  # change color to cyan
+			PS1="$PS1"'`__git_ps1`'   # bash function
+		fi
+	fi
+	PS1="$PS1"'\[\033[0m\]'        # change color
+	PS1="$PS1"'\n'                 # new line
+	PS1="$PS1"'$ '                 # prompt: always $
+fi
+
+MSYS2_PS1="$PS1"               # for detection by MSYS2 SDK's bash.basrc
+
+# Evaluate all user-specific Bash completion scripts (if any)
+if test -z "$WINELOADERNOEXEC"
+then
+	for c in "$HOME"/bash_completion.d/*.bash
+	do
+		# Handle absence of any scripts (or the folder) gracefully
+		test ! -f "$c" ||
+		. "$c"
+	done
+fi
